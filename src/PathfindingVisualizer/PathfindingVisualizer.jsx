3a6f0ab9f3dba4e09cc3b9cb003bf01b123a8285
rohit jain <rohit.bbd67@gmail.com>
2024-11-01 08:12:11 +0530

    first commit

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from 'C:/Users/suraj/Desktop/pathFinder/Pathfinding/src/PathfindingVisualizer/gfg-new-logo.png';
 import Node from './Node/Node';
 import {dijkstra, getNodesInShortestPathOrder} from '../algorithms/dijkstra';
 
@@ -79,9 +78,11 @@ export default class PathfindingVisualizer extends Component {
 
     return (
       <>
-      <div className="logo">
+      {/* <div className="logo">
           <img src={logo} width="100" height="50" />
-        </div>
+        </div> */}
+        <br/>
+        <br/>
         <button onClick={() => this.visualizeDijkstra()}>
           Visualize Dijkstra's Algorithm
         </button>
